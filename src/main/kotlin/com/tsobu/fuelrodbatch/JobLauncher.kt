e884dd4f3d4c1f1a8bc1aec1924b772f85986fb4
Sammy B <barsamms@gmail.com>
2020-08-21 17:15:47 +0300

    Updated stuff

--- src/main/kotlin/com/tsobu/fuelrodbatch/JobLauncher.kt
+++ src/main/kotlin/com/tsobu/fuelrodbatch/JobLauncher.kt
@@ -1,17 +1,10 @@
 package com.tsobu.fuelrodbatch
 
-import com.tsobu.fuelrodbatch.common.Tweet
-import org.jeasy.batch.core.filter.HeaderRecordFilter
-import org.jeasy.batch.core.job.JobBuilder
-import org.jeasy.batch.core.job.JobExecutor
-import org.jeasy.batch.core.writer.StandardOutputRecordWriter
-import org.jeasy.batch.flatfile.DelimitedRecordMapper
-import org.jeasy.batch.flatfile.FlatFileRecordReader
-import org.jeasy.batch.validation.BeanValidationRecordValidator
+import org.jobrunr.configuration.JobRunr
+import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider
+import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider.DatabaseOptions
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.nio.file.Paths
-
 
 object JobLauncher {
 
@@ -19,19 +12,10 @@ object JobLauncher {
     @JvmStatic
     fun main(args: Array<String>) {
         val logger: Logger = LoggerFactory.getLogger(JobLauncher::class.java)
-        val dataSource = Paths.get("tweets.csv")
 
-
-        val job = JobBuilder()
-            .named("fuelrod-message-queue")
-            .reader(FlatFileRecordReader(dataSource))
-            .mapper(DelimitedRecordMapper(Tweet::class.java, "id", "name", "message"))
-            .validator(BeanValidationRecordValidator())
-            .writer(StandardOutputRecordWriter())
-            .build()
-
-        val jobExecutor = JobExecutor()
-        val report = jobExecutor.execute(job)
-        jobExecutor.shutdown()
+        JobRunr.configure()
+            .useStorageProvider(DefaultSqlStorageProvider(dataSource, DatabaseOptions.SKIP_CREATE))
+            .useDefaultBackgroundJobServer()
+            .initialize()
     }
 }
